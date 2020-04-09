e2674599e572cab36d39ba88a611c3595a1ef904
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-04-09 19:23:51 +0200

    Remove in js class active from links and articles

--- js/script.js
+++ js/script.js
@@ -12,11 +12,26 @@ const titleClickHandler = function(event){
 	console.log('Link was clicked');
 
 	/* remove class 'active' from all article links*/
-	
+	const activeLinks = document.querySelectorAll('.titles a.active');
+
+	for(let activeLink of activeLinks) {
+
+		activeLink.classList.remove('active');
+
+		console.log('usuń ' + 'active');
+	}
 	/* add class'active' to the clicked link */
-	
+
 	/* remove class'active' from all articles*/
-	
+	const activeArticles = document.querySelectorAll('.posts .post.active');
+
+	for(let activeArticle of activeArticles) {
+
+		activeArticle.classList.remove('active');
+		
+		console.log('usuń artykuł ' + 'active');
+	}
+
 	/* get 'href' attribute from the clicked link*/
 
 	/* find the correct article using the selector (value of 'href' attribute) */
