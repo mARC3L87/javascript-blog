e4fd38cb5c8fe0219574a17ad9da81225eb2489c
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-04-09 19:42:59 +0200

    Add class active to the clicked link in js

--- js/script.js
+++ js/script.js
@@ -7,28 +7,33 @@
 
 const titleClickHandler = function(event){
 
-	console.log(event);
+	const clickedElement = this;
+
+	/* console.log(event); */
 
 	console.log('Link was clicked');
 
-	/* remove class 'active' from all article links*/
+	/* [DONE] remove class 'active' from all article links*/
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks) {
 
 		activeLink.classList.remove('active');
 
-		console.log('usuń ' + 'active');
+		console.log('usuń link ' + 'active');
 	}
 	/* add class'active' to the clicked link */
+	clickedElement.classList.add('active');
 
-	/* remove class'active' from all articles*/
+	console.log('clickedElement: ', clickedElement);
+
+	/* [DONE} remove class'active' from all articles*/
 	const activeArticles = document.querySelectorAll('.posts .post.active');
 
 	for(let activeArticle of activeArticles) {
 
 		activeArticle.classList.remove('active');
-		
+
 		console.log('usuń artykuł ' + 'active');
 	}
 
